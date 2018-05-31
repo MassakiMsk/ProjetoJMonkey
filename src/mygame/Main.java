f5139854353b97937db45f2731185d300810fc22
Massaki Msk <alisonmassakicheno@hotmail.com>
2018-05-31 00:58:59 -0300

    Resolve conflot no git
    Misturo tudo :c

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -120,7 +120,6 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     @Override
     public void collision(PhysicsCollisionEvent event) {
         if (event.getNodeA().getName().equals("tuc") || event.getNodeB().getName().equals("tuc")) {
-<<<<<<< HEAD
             if(event.getNodeA().getName().equals("shot") || event.getNodeB().getName().equals("shot")) {
                 rootNode.detachChild(event.getNodeA());
                 bulletAppState.getPhysicsSpace().remove(event.getNodeA().getControl(RigidBodyControl.class));
@@ -141,13 +140,6 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
                 rootNode.detachChild(event.getNodeB());
                 bulletAppState.getPhysicsSpace().remove(event.getNodeB().getControl(RigidBodyControl.class));
                 tucs.remove(event.getNodeB());
-=======
-            if(event.getNodeA().getName().equals("shot")) {
-
-            }
-            else if(event.getNodeB().getName().equals("shot")) {
-                
->>>>>>> 4fb2922be18c9de1db77e459b3293338aea09654
             }
         }
     }
@@ -244,145 +236,15 @@ public class Main extends SimpleApplication implements ActionListener, PhysicsCo
     public void createTuc() {
         tuc = new Tuc(bulletAppState, assetManager, bus);
         
-<<<<<<< HEAD
-=======
-        Box boxMesh;
-        Geometry boxGeo;
-        Material boxMat;
-        
-        boxMesh = new Box(0.5f, 0.5f, 0.5f);
-        boxGeo = new Geometry("Box", boxMesh);
-        boxMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        boxMat.setColor("Color", ColorRGBA.Red);
-        boxGeo.setName("tuc");
-        boxGeo.setMaterial(boxMat);
-        tuc.attachChild(boxGeo);
-        
-        tuc.setLocalTranslation(bus.getLocalTranslation());
-        
-        RigidBodyControl boxPhysicsNode = new RigidBodyControl(0);
-        tuc.addControl(boxPhysicsNode);
-        bulletAppState.getPhysicsSpace().add(boxPhysicsNode);
-        
->>>>>>> 4fb2922be18c9de1db77e459b3293338aea09654
         tucs.add(tuc);
         rootNode.attachChild(tuc);
     }
     
-<<<<<<< HEAD
-=======
-    public void updateTuc(float tpf) {
-        
-        double tX, tZ, pX, pZ, auxauxTheta;
-        
-
-        
-        for(Node currentTuc : tucs){
-            tZ = currentTuc.getLocalTranslation().z;
-            tX = currentTuc.getLocalTranslation().x;
-            pX = player.getLocalTranslation().x;
-            pZ = player.getLocalTranslation().z;
-
-            double theta = Math.atan(Math.abs((tZ - pZ) / (tX - pX)));
-            if(tX < pX) {
-                if(tZ > pZ) {
-                    theta = (1.5078 * 2) - theta;
-                }
-                else {
-                    theta = (1.5078 * 2) + theta;
-                }
-            }
-            else {
-                if(tZ < pZ) {
-                    theta = (1.5078 * 4) - theta;
-                }
-            }
-            quat.fromAngleAxis((float)theta, new Vector3f(0, -1, 0));
-            currentTuc.setLocalRotation(quat);
-            currentTuc.setLocalTranslation((float)(currentTuc.getLocalTranslation().x - (Math.cos(theta) * tpf)), (float)currentTuc.getLocalTranslation().y, (float)(currentTuc.getLocalTranslation().z - (Math.sin(theta) * tpf)));
-            currentTuc.getControl(RigidBodyControl.class).setPhysicsLocation(currentTuc.getLocalTranslation());
-            currentTuc.getControl(RigidBodyControl.class).setPhysicsRotation(currentTuc.getLocalRotation());
-        }
-    }
-    
-    public void updatePlayer(float tpf) {
-        double cX, cY, pX, pY, auxauxTheta;
-        
-        cX = inputManager.getCursorPosition().x;
-        cY = inputManager.getCursorPosition().y;
-        pX = cam.getScreenCoordinates(player.getLocalTranslation()).x;
-        pY = cam.getScreenCoordinates(player.getLocalTranslation()).y;
-        double theta = Math.atan(Math.abs((cY - pY) / (cX - pX)));
-        
-        if(cX < pX) {
-            if(cY > pY) {
-                theta = (1.5078 * 2) - theta;
-            }
-            else {
-                theta = (1.5078 * 2) + theta;
-            }
-        }
-        else {
-            if(cY < pY) {
-                theta = (1.5078 * 4) - theta;
-            }
-        }
-        
-        auxauxTheta = theta * 180 / Math.PI;
-        quat.fromAngleAxis((float)theta, new Vector3f(0, 1, 0));
-        player.setLocalRotation(quat);
-        player.getControl(RigidBodyControl.class).setPhysicsRotation(player.getLocalRotation());
-        player.getControl(RigidBodyControl.class).activate();
-        
-        if(up) {
-            player.setLocalTranslation((float)(player.getLocalTranslation().x - (Math.cos(theta) * tpf * 2)), (float)player.getLocalTranslation().y, (float)(player.getLocalTranslation().z + (Math.sin(theta) * tpf * 2)));
-            player.getControl(RigidBodyControl.class).setPhysicsLocation(player.getLocalTranslation());
-        }
-        else if(down) {
-            player.setLocalTranslation((float)(player.getLocalTranslation().x + (Math.cos(theta) * tpf * 2)), (float)player.getLocalTranslation().y, (float)(player.getLocalTranslation().z - (Math.sin(theta) * tpf * 2)));
-            player.getControl(RigidBodyControl.class).setPhysicsLocation(player.getLocalTranslation());
-        }
-        
-        auxTheta = theta;
-    }
-    
->>>>>>> 4fb2922be18c9de1db77e459b3293338aea09654
     public void createShot() {
         shot = new Shot(bulletAppState, assetManager, player);
         
-<<<<<<< HEAD
-=======
-        boxMesh = new Box(0.3f, 0.1f, 0.1f);
-        boxGeo = new Geometry("Box", boxMesh);
-        boxMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        boxMat.setColor("Color", ColorRGBA.Yellow);
-        boxGeo.setName("shot");
-        boxGeo.setMaterial(boxMat);
-        shot.attachChild(boxGeo);
-        
-        shot.setLocalTranslation(player.getLocalTranslation());
-        shot.setLocalRotation(player.getLocalRotation());
-        
-        RigidBodyControl boxPhysicsNode = new RigidBodyControl(0);
-        shot.addControl(boxPhysicsNode);
-        bulletAppState.getPhysicsSpace().add(boxPhysicsNode);
-        
->>>>>>> 4fb2922be18c9de1db77e459b3293338aea09654
         shots.add(shot);
         rootNode.attachChild(shot);
     }
     
-<<<<<<< HEAD
-=======
-    public void updateShot(float tpf) {
-        int i = 0;
-        double theta;
-        for(Node currentShot : shots) {
-            theta = thetas.get(i);
-            i++;
-            currentShot.setLocalTranslation((float)(currentShot.getLocalTranslation().x - Math.cos(theta) * tpf * 5), currentShot.getLocalTranslation().y, (float)(currentShot.getLocalTranslation().z + Math.sin(theta) * tpf * 5));
-            currentShot.getControl(RigidBodyControl.class).setPhysicsLocation(currentShot.getLocalTranslation());
-        }
-    }
->>>>>>> 4fb2922be18c9de1db77e459b3293338aea09654
 }
